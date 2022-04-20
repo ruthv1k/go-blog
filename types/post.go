c3eadeec6df74b5161309b1c61683b1ba6de057b
ruthvik <ruthvik.local>
2022-04-20 11:51:15 +0530

    I-1: Change PostId type to string from primitive.ObjectId

--- types/post.go
+++ types/post.go
@@ -1,9 +1,7 @@
 package types
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type PublicPost struct {
-	PostId primitive.ObjectID `bson:"_id,omitempty" json:"post_id"`
+	PostId string `json:"post_id"`
 	AuthorId string `json:"author_id"`
 	Title string `json:"title"`
 	Description string `json:"description"`
